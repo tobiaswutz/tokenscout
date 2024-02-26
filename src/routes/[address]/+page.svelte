ecd5b8b26abb7331be2f663a4ec6864da028a46a
tobiaswutz <tobiaswutz22@gmail.com>
2024-02-26 23:02:54 +0100

    animations

--- src/routes/[address]/+page.svelte
+++ src/routes/[address]/+page.svelte
@@ -72,17 +72,25 @@
 		tokenA: trade.tokenAName,
 		tokenAAmount: trade.tokenAAmount.toFixed(2),
 		tokenB: trade.tokenBName,
-		tokenBAmount: trade.tokenBAmount.toFixed(2),
+		tokenBAmount: trade.tokenBAmount.toFixed(2)
 	}));
 
 	const tableSimple: TableSource = {
 		head: ['Ticker', 'Balance', 'Preis', 'ADA-Wert'],
-		body: tableMapperValues(formattedData, ['ticker', 'balance', 'price', 'adaValue']),
+		body: tableMapperValues(formattedData, ['ticker', 'balance', 'price', 'adaValue'])
 	};
 
 	const tableTrades: TableSource = {
 		head: ['Aktion', 'Börse', 'Zeit', 'Token A', 'Menge A', 'Token B', 'Menge B'],
-		body: tableMapperValues(formattedTrades, ['action', 'exchange', 'time', 'tokenA', 'tokenAAmount', 'tokenB', 'tokenBAmount']),
+		body: tableMapperValues(formattedTrades, [
+			'action',
+			'exchange',
+			'time',
+			'tokenA',
+			'tokenAAmount',
+			'tokenB',
+			'tokenBAmount'
+		])
 	};
 </script>
 
@@ -99,14 +107,50 @@
 		</div>
 	</div>
 
-    <div>
-        <h3 class="text-2xl mb-2">Portfolio:</h3>
-        <Table source={tableSimple} />
-    </div>
+	<section class="img-bg" />
 
-    <div class="">        
-        <h3 class="text-2xl mb-2">Trades:</h3>
-        <Table source={tableTrades} />
-    </div>
+	<div>
+		<h3 class="text-2xl mb-2">Portfolio:</h3>
+		<Table source={tableSimple} />
+	</div>
 
+	<div class="">
+		<h3 class="text-2xl mb-2">Trades:</h3>
+		<Table source={tableTrades} />
+	</div>
 </div>
+
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
